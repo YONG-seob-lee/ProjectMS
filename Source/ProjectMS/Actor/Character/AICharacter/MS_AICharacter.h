2a811c33134b07ea2b258d9052433c14fd993697
JJL324 <ji2137311@gmail.com>
2024-12-18 03:51:18 +0900

    [이재준] Add - [AI 행동 패턴 규격화]

--- Source/ProjectMS/Actor/Character/AICharacter/MS_AICharacter.h
+++ Source/ProjectMS/Actor/Character/AICharacter/MS_AICharacter.h
@@ -4,6 +4,32 @@
 #include "Actor/Character/MS_CharacterBase.h"
 #include "MS_AICharacter.generated.h"
 
+UENUM(BlueprintType) enum class EMS_AIBehaviorPattern : uint8
+{
+	Undefined		UMETA(DisplayName = "Undefined"),
+	Navigate		UMETA(DisplayName = "Navigate"),
+	Operate			UMETA(DisplayName = "Operate"),
+	Observe			UMETA(DisplayName = "Observe"),
+	Interact		UMETA(DisplayName = "Operate"),
+	Idle			UMETA(DisplayName = "Idle")
+};
+
+/* AIBehaviorPattern
+* 
+*	Navigate: Move to location
+*
+*	Operate: Manipulate the environment or interact with objects
+*	Ex: Pick up the items from the storage
+* 
+*	Observe: Identify the situation and determine the required task
+*	Ex: Find empty storage
+* 
+*	Interact: Tasks that interact with other AIs or objects
+*	Ex: Payment, cooperation, conversation with customers.
+* 
+*	Idle : Idle
+*/
+
 UCLASS() class PROJECTMS_API AMS_AICharacter : public AMS_CharacterBase
 {
 	GENERATED_BODY()
@@ -21,4 +47,8 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) class UCapsuleComponent* CollisionCapsuleComponent = nullptr;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) class USkeletalMeshComponent* SkeletalMeshComponent = nullptr;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite) class UCharacterMovementComponent* CharacterMovementComponent = nullptr;
+
+	// Property
+public:
+	EMS_AIBehaviorPattern AIBehaviorPattern = EMS_AIBehaviorPattern::Undefined;
 };
--- Source/ProjectMS/Actor/Character/AICharacter/StaffAICharacter/MS_StaffAICharacter.h
+++ Source/ProjectMS/Actor/Character/AICharacter/StaffAICharacter/MS_StaffAICharacter.h
@@ -4,6 +4,21 @@
 #include "Actor/Character/AICharacter/MS_AICharacter.h"
 #include "MS_StaffAICharacter.generated.h"
 
+UENUM(BlueprintType) enum class EMS_StaffAIObservePattern : uint8
+{
+	Undefined			UMETA(DisplayName = "Undefined"),
+	Inspection			UMETA(DisplayName = "Inspection")
+};
+
+UENUM(BlueprintType) enum class EMS_StaffAIOperatePattern : uint8
+{
+	Undefined			UMETA(DisplayName = "Undefined"),
+	Fetch				UMETA(DisplayName = "Fetch"),
+	Pick				UMETA(DisplayName = "Pick"),
+	Drop				UMETA(DisplayName = "Drop"),
+};
+
+
 UCLASS() class PROJECTMS_API AMS_StaffAICharacter : public AMS_AICharacter
 {
 	GENERATED_BODY()
@@ -14,4 +29,9 @@ public:
 	virtual void PostInitializeComponents() override;
 	virtual void BeginPlay() override;
 	virtual void Tick(float aDeltaTime) override;
+
+	// Property
+public:
+	EMS_StaffAIObservePattern StaffAIObservePattern = EMS_StaffAIObservePattern::Undefined;
+	EMS_StaffAIOperatePattern StaffAIOperatePattern = EMS_StaffAIOperatePattern::Undefined;
 };
