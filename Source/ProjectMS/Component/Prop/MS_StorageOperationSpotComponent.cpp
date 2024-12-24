b1a7bfdef53cf052dc4097e8db66b2cf5cfee760
JJL324 <ji2137311@gmail.com>
2024-12-25 02:40:13 +0900

    [이재준] Fix - [.idea 삭제 사고 처리]

---
+++ Source/ProjectMS/Component/Prop/MS_StorageOperationSpotComponent.cpp
@@ -0,0 +1,185 @@
+#include "Component/Prop/MS_StorageOperationSpotComponent.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Components/LineBatchComponent.h"
+
+#include "Actor/Storage/MS_Storage.h"
+
+UMS_StorageOperationSpotComponent::UMS_StorageOperationSpotComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	SpotSize.X = SpotSize.Y = 25.0f;
+	SpotSize.Z = 5.0f;
+
+	AssemblyAreaSize.X = 100.0f;
+	AssemblyAreaSize.Y = 100.0f;
+	AssemblyAreaSize.Z = 100.0f;
+}
+
+
+void UMS_StorageOperationSpotComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	OwnerStorage = Cast<AMS_Storage>(GetOwner());
+	MS_CHECK(OwnerStorage);
+}
+
+#if WITH_EDITOR
+void UMS_StorageOperationSpotComponent::OnComponentCreated()
+{
+	Super::OnComponentCreated();
+
+	DrawSpotOutline();
+	DrawAssemblyAreaOutline();
+}
+
+void UMS_StorageOperationSpotComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+	Super::OnComponentDestroyed(bDestroyingHierarchy);
+
+	EraseSpotOutline();
+	EraseAssemblyAreaOutline();
+}
+
+void UMS_StorageOperationSpotComponent::OnRegister()
+{
+	Super::OnRegister();
+
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+	}
+
+	RedrawSpotOutline();
+	RedrawAssemblyAreaOutline();
+}
+
+void UMS_StorageOperationSpotComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	FName ChangedMemberPropertyName = (PropertyChangedEvent.MemberProperty != nullptr ? PropertyChangedEvent.MemberProperty->GetFName() : NAME_None);
+	if (ChangedMemberPropertyName == FName("RelativeLocation") || ChangedMemberPropertyName == FName("RelativeRotation") || ChangedMemberPropertyName == FName("SpotOutlineColor") || ChangedMemberPropertyName == FName("SpotOutlineThickness") || ChangedMemberPropertyName == FName("AssemblyAreaOutlineColor") || ChangedMemberPropertyName == FName("AssemblyAreaOutlineThickness"))
+	{
+		if (GetWorld() != nullptr)
+		{
+			GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+		}
+
+		RedrawSpotOutline();
+		RedrawAssemblyAreaOutline();
+	}
+	else if (ChangedMemberPropertyName == FName("SpotSize"))
+	{
+		if (GetWorld() != nullptr)
+		{
+			GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+		}
+
+		RedrawSpotOutline();
+		RedrawAssemblyAreaOutline();
+	}
+}
+
+void UMS_StorageOperationSpotComponent::PostEditComponentMove(bool bFinished)
+{
+	Super::PostEditComponentMove(bFinished);
+
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+	}
+
+	RedrawSpotOutline();
+	RedrawAssemblyAreaOutline();
+
+}
+
+void UMS_StorageOperationSpotComponent::PostEditUndo()
+{
+	Super::PostEditUndo();
+
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+	}
+
+	RedrawSpotOutline();
+	RedrawAssemblyAreaOutline();
+}
+
+void UMS_StorageOperationSpotComponent::DrawSpotOutline()
+{
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->DrawCylinder(GetComponentLocation() + FVector(0.0f, 0.0f, SpotSize.Z), GetComponentLocation(), (SpotSize.X + SpotSize.Y) / 4.0f, 8, SpotOutlineColor, -1.0f, SDPG_World, SpotOutlineThickness, GetUniqueID());
+	}
+}
+
+void UMS_StorageOperationSpotComponent::RedrawSpotOutline()
+{
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->DrawCylinder(GetComponentLocation() + FVector(0.0f, 0.0f, SpotSize.Z), GetComponentLocation(), (SpotSize.X + SpotSize.Y) / 4.0f, 8, SpotOutlineColor, -1.0f, SDPG_World, SpotOutlineThickness, GetUniqueID());
+	}
+}
+
+void UMS_StorageOperationSpotComponent::EraseSpotOutline()
+{
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+	}
+}
+
+void UMS_StorageOperationSpotComponent::DrawAssemblyAreaOutline()
+{
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->DrawBox(GetComponentLocation(), AssemblyAreaSize / 2.0f, GetComponentRotation().Quaternion(), AssemblyAreaOutlineColor, -1.0f, SDPG_World, AssemblyAreaOutlineThickness, GetUniqueID());
+	}
+}
+
+void UMS_StorageOperationSpotComponent::RedrawAssemblyAreaOutline()
+{
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->DrawBox(GetComponentLocation() + FVector(0.0f, 0.0f, AssemblyAreaSize.Z / 2.0f), AssemblyAreaSize / 2.0f, GetComponentRotation().Quaternion(), AssemblyAreaOutlineColor, -1.0f, SDPG_World, AssemblyAreaOutlineThickness, GetUniqueID());
+	}
+}
+
+void UMS_StorageOperationSpotComponent::EraseAssemblyAreaOutline()
+{
+	if (GetWorld() != nullptr)
+	{
+		GetWorld()->PersistentLineBatcher->ClearBatch(GetUniqueID());
+	}
+}
+
+#endif
+
+void UMS_StorageOperationSpotComponent::ReserveWorker(AMS_StaffAICharacter* aStaffCharacter)
+{
+	ReservationFlag = true;
+	ReservationWorker = aStaffCharacter;
+}
+
+void UMS_StorageOperationSpotComponent::UnreserveWorker()
+{
+	ReservationFlag = false;
+	ReservationWorker = nullptr;
+}
+
+void UMS_StorageOperationSpotComponent::DeployWorker(AMS_StaffAICharacter* aStaffCharacter)
+{
+	OccupancyFlag = true;
+	OccupancyWorker = aStaffCharacter;
+}
+
+void UMS_StorageOperationSpotComponent::UndeployWorker()
+{
+	OccupancyFlag = false;
+	OccupancyWorker = nullptr;
+}
---
+++ Source/ProjectMS/Component/Prop/MS_StorageOperationSpotComponent.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "MS_StorageOperationSpotComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) ) class PROJECTMS_API UMS_StorageOperationSpotComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UMS_StorageOperationSpotComponent();
+
+	virtual void InitializeComponent() override;
+#if WITH_EDITOR
+	virtual void OnComponentCreated() override;
+	virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+	virtual void OnRegister() override;
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void PostEditComponentMove(bool bFinished) override;
+	virtual void PostEditUndo() override;
+
+private:
+	void DrawSpotOutline();
+	void RedrawSpotOutline();
+	void EraseSpotOutline();
+
+	void DrawAssemblyAreaOutline();
+	void RedrawAssemblyAreaOutline();
+	void EraseAssemblyAreaOutline();
+#endif
+
+	UFUNCTION(BlueprintCallable) void ReserveWorker(class AMS_StaffAICharacter* aStaffCharacter);
+	UFUNCTION(BlueprintCallable) void UnreserveWorker();
+	UFUNCTION(BlueprintCallable) void DeployWorker(class AMS_StaffAICharacter* aStaffCharacter);
+	UFUNCTION(BlueprintCallable) void UndeployWorker();
+
+	// Instance
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) TObjectPtr<class AMS_Storage> OwnerStorage = nullptr;
+
+	// Property
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int SpotOrder = INT_MIN;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FVector SpotSize = {};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FColor SpotOutlineColor = FColor::Purple;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float SpotOutlineThickness = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FVector AssemblyAreaSize = {};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FColor AssemblyAreaOutlineColor = FColor::Turquoise;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) float AssemblyAreaOutlineThickness = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) bool OccupancyFlag = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) TObjectPtr<class AMS_StaffAICharacter> OccupancyWorker = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) bool ReservationFlag = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) TObjectPtr<class AMS_StaffAICharacter> ReservationWorker = nullptr;
+};
