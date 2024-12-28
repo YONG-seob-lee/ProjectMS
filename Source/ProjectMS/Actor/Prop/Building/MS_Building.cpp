fdc6891ff224c385a05b3357d4ae63762fce14c9
YONG-seob-lee <64522839+YONG-seob-lee@users.noreply.github.com>
2024-12-28 12:19:56 +0900

    [이용섭] Add - [Building] 건물 층수를 가변적이게 변경할 수 있는 블루프린트 추가.

---
+++ Source/ProjectMS/Actor/Prop/Building/MS_Building.cpp
@@ -0,0 +1,118 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MS_Building.h"
+
+AMS_Building::AMS_Building()
+{
+	BuildingFront = CreateDefaultSubobject<UStaticMeshComponent>("BuildingFront");
+	if(BuildingFront)
+	{
+		SetRootComponent(BuildingFront);
+	}
+
+	BuildingMiddle = CreateDefaultSubobject<UStaticMeshComponent>("BuildingMid");
+	if(BuildingMiddle)
+	{
+		BuildingMiddle->AttachToComponent(BuildingFront, FAttachmentTransformRules::KeepRelativeTransform);
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_1"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_2"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_3"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_4"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_5"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_6"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_7"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_8"));
+		MiddleArray.Emplace(CreateDefaultSubobject<UStaticMeshComponent>("Middle_9"));
+	}
+	for(const auto& Middle : MiddleArray)
+	{
+		Middle->SetVisibility(false, true);
+	}
+	
+	BuildingRear = CreateDefaultSubobject<UStaticMeshComponent>("BuildingRear");
+	if(BuildingRear)
+	{
+		BuildingRear->AttachToComponent(BuildingFront, FAttachmentTransformRules::KeepRelativeTransform);
+	}
+
+	SetFloor(Floor);
+}
+
+void AMS_Building::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	UpdateFloorSize();
+	SetFloor(Floor);
+}
+
+void AMS_Building::UpdateFloorSize()
+{
+	if(BuildingFront->GetStaticMesh())
+	{
+		MiddleStartLocationZ = BuildingFront->GetStaticMesh()->GetBounds().Origin.Z * 2.f;
+	}
+
+	if(BuildingMiddle->GetStaticMesh())
+	{
+		MiddleSizeZ = BuildingMiddle->GetStaticMesh()->GetBounds().Origin.Z * 2.f;
+	}
+
+	for(const auto& Middle : MiddleArray)
+	{
+		Middle->SetStaticMesh(BuildingMiddle->GetStaticMesh());
+	}
+}
+
+void AMS_Building::SetFloor(int32 aFloor)
+{
+	switch(aFloor)
+	{
+	case 0:
+		{
+			BuildingFront->SetVisibility(false, true);
+			BuildingMiddle->SetVisibility(false, true);
+			BuildingRear->SetVisibility(false, true);
+			break;
+		}
+	case 1:
+		{
+			BuildingFront->SetVisibility(true, true);
+			BuildingMiddle->SetVisibility(false, true);
+			BuildingRear->SetVisibility(true, true);
+			BuildingRear->SetRelativeLocation(FVector(0.f, 0.f, MiddleStartLocationZ));
+			break;
+		}
+	default:
+		{
+			BuildingFront->SetVisibility(true, true);
+			BuildingRear->SetVisibility(true, true);
+			BuildingMiddle->SetVisibility(true, true);
+
+			for(const auto& Middle : MiddleArray)
+			{
+				Middle->SetVisibility(false, true);
+			}
+
+			for(int32 i = 0 ; i < aFloor - 2 ; i++)
+			{
+				if(MiddleArray.IsValidIndex(i))
+				{
+					MiddleArray[i]->SetVisibility(true,  true);
+					MiddleArray[i]->AttachToComponent(BuildingMiddle, FAttachmentTransformRules::KeepRelativeTransform);
+					MiddleArray[i]->SetRelativeLocation(FVector(0.f, 0.f, MiddleSizeZ * (i + 1)));
+				}
+			}
+			
+			BuildingMiddle->SetRelativeLocation(FVector(0.f, 0.f, MiddleStartLocationZ));
+
+			if(aFloor <= MiddleArray.Num() + 2)
+			{
+				BuildingRear->SetRelativeLocation(FVector(0.f, 0.f, MiddleStartLocationZ + MiddleSizeZ * (aFloor - 1)));
+			}
+			
+			break;
+		}
+	}
+}
---
+++ Source/ProjectMS/Actor/Prop/Building/MS_Building.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Prop/MS_Prop.h"
+#include "MS_Building.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class PROJECTMS_API AMS_Building : public AMS_Prop
+{
+	GENERATED_BODY()
+public:
+	AMS_Building();
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	
+private:
+	void UpdateFloorSize();
+	void SetFloor(int32 aFloor);
+	
+	UPROPERTY(EditAnywhere, Category = "Floor", meta = (AllowPrivateAccess = "true"))
+	int32 Floor = 0;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Floor", meta = (AllowPrivateAccess = "true"))
+	float MiddleStartLocationZ = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Floor", meta = (AllowPrivateAccess = "true"))
+	float MiddleSizeZ = 0.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Floor", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> BuildingFront = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Floor", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> BuildingMiddle = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Floor", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> BuildingRear = nullptr;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UStaticMeshComponent>> MiddleArray;
+};
