ca4c02e5a2bbb47441fd132ca57caa3dfd7b6db3
YONG-seob-lee <dydtjql12@naver.com>
2025-01-11 00:08:18 +0900

    [이용섭] Add - [SoundManager] 클래스 추가.

---
+++ Source/ProjectMS/Management/Manager_Client/MS_SoundManager.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MS_SoundManager.h"
+
+UMS_SoundManager::UMS_SoundManager()
+{
+}
---
+++ Source/ProjectMS/Management/Manager_Client/MS_SoundManager.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MS_ManagerBase.h"
+#include "MS_SoundManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMS_API UMS_SoundManager : public UMS_ManagerBase
+{
+	GENERATED_BODY()
+public:
+	UMS_SoundManager();
+
+private:
+	UPROPERTY(EditAnywhere) TObjectPtr<class USoundMix> SoundMix = nullptr; 
+};
