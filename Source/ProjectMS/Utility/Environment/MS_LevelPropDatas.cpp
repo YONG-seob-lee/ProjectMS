c89687daa8eb5205b8e39ce63412b89483ea6fa0
capark2690 <capark2690@gmail.com>
2025-01-25 23:51:24 +0900

    [박청아] Fix - [Construct] e7ecc5a 작업 중 삭제된 부분 복구

--- Source/ProjectMS/Utility/Environment/MS_LevelPropDatas.cpp
+++ Source/ProjectMS/Utility/Environment/MS_LevelPropDatas.cpp
@@ -1,1 +1,36 @@
 ﻿#include "MS_LevelPropDatas.h"
+
+#include "MS_Define.h"
+
+
+const FVector FMS_GridData::GetGridLocation() const
+{
+	if (OwnerZone == nullptr)
+	{
+		MS_LOG_VERBOSITY(Error, TEXT("[%s] OwnerZone is null"), *MS_FUNC_STRING);
+
+		return FVector(MS_GridSize.X * static_cast<float>(GridPosition.X)
+			, MS_GridSize.Y * static_cast<float>(GridPosition.Y)
+			, 0.f);
+	}
+		
+	return FVector(MS_GridSize.X * static_cast<float>(GridPosition.X)
+		, MS_GridSize.Y * static_cast<float>(GridPosition.Y)
+		, OwnerZone->GetActorLocation().Z);
+}
+
+const FVector FMS_GridData::GetGridCenterLocation() const
+{
+	if (OwnerZone == nullptr)
+	{
+		MS_LOG_VERBOSITY(Error, TEXT("[%s] OwnerZone is null"), *MS_FUNC_STRING);
+
+		return FVector(MS_GridSize.X * static_cast<float>(GridPosition.X) + MS_GridSize.X / 2.f
+			, MS_GridSize.Y * static_cast<float>(GridPosition.Y) + MS_GridSize.Y / 2.f
+			, 0.f);
+	}
+	
+	return FVector(MS_GridSize.X * static_cast<float>(GridPosition.X) + MS_GridSize.X / 2.f
+		, MS_GridSize.Y * static_cast<float>(GridPosition.Y) + MS_GridSize.Y / 2.f
+		, OwnerZone->GetActorLocation().Z);
+}
